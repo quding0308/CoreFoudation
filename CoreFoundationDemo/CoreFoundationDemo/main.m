8e21d127ab858f2cb36b47469e8ef88582a94c1c
quding <ding_qu@kingdee.com>
2019-01-08 15:37:56 +0800

    1

---
+++ CoreFoundationDemo/CoreFoundationDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CoreFoundationDemo
+//
+//  Created by hour on 2019/1/8.
+//  Copyright © 2019 hour. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
